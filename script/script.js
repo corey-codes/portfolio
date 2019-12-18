ed78bd63d25b5271962f99102e15b48db03aaac1
hellcat04 <coreyderouin@outlook.com>
2019-12-18 14:38:09 -0500

    updated styling,removed form :bomb:

---
+++ script/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.header-nav__hamburger').on('click', function () {
+        // toggle class show on the menu
+        $('.header-nav__list').toggleClass('show');
+    });
+
+    $('.header-nav__exit-responsive').on('click', function () {
+        // toggle class show on the menu
+        $('.header-nav__list').toggleClass('show');
+    });
+    // console.log("TESTING!");
+});
