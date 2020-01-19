620f2d45fb14fe935e657052b01acad069635d4c
hellcat04 <coreyderouin@outlook.com>
2020-01-18 20:55:43 -0500

    mobile nav added

--- script/script.js
+++ script/script.js
@@ -1,12 +1,35 @@
-$(document).ready(function () {
-    $('.hamburger').on('click', function () {
-        // toggle class show on the menu
-        $('.header-nav__menu').toggleClass('show');
-    });
+const mobileShow = () => {
+    
+    const mobile = document.querySelector('.header-nav__mobileNav');
+    const navMenu = document.querySelector('.header-nav__menu');  
+    const navLinks = document.querySelectorAll('.header-nav__item');
+    const body = document.querySelector('body');
+    
+    //  OPEN MOBILE MENU
+    //  CLOSE ON HAMBURGER MENU CLICK OR LINK SELECTION
+    body.addEventListener('click', event => {
+        if (event.target == mobile || navLinks) {
+            navMenu.classList.toggle('header-nav__menu--mobileActive');
+        }
 
-    $('.exit-responsive').on('click', function () {
-        // toggle class show on the menu
-        $('.header-nav__menu').toggleClass('show');
+        // ANIMATE LINKS ON LOAD
+        navLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = "";
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.7}s`
+            }
+        })
+
+        // BURGER ANIMATION
+        mobile.classList.toggle('header-nav__burger--toExit');
+
     });
-    // console.log("TESTING!");
-});
+}
+mobileShow();
+
+
+
+
+
+
