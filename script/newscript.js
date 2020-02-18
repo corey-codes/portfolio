6763ea84d07618ed3e51c45261e944dd13f11f7f
hellcat04 <coreyderouin@outlook.com>
2020-02-18 09:52:05 -0500

    fixed broken link & adjusted mobile nav

--- script/newscript.js
+++ script/newscript.js
@@ -1,32 +1,36 @@
 /* ==========================================
 
-MOBILE NAV
+MOBILE 
 
 ========================================== */
+
+
+// UI CONTROLLERS
+//***************************************
 
 const mobileOpen = () => {
 
     // HTML ELEMENTS **********
     const headerNav = document.querySelector('.header-nav');
-    console.log(headerNav);
+        console.log(headerNav);
 
     const mobileNav = document.querySelector('.header-nav__mobileNav');
-    console.log(mobileNav);
+        console.log(mobileNav);
 
     const navMenu = document.querySelector('.header-nav__menu');
-    console.log(navMenu);
+        console.log(navMenu);
 
     const navLinks = document.querySelectorAll('.header-nav__item');
-    console.log(navLinks);
+        console.log(navLinks);
 
     // ARRAY WITH BOTH MOBILE NAV & LINKS
     const mobileClickOptions = [];
-    console.log(mobileClickOptions);
-    mobileClickOptions.push(navMenu, navLinks)
-
+        console.log(mobileClickOptions);
+    mobileClickOptions.push(navMenu, navLinks) 
+    
     headerNav.addEventListener('click', () => {
-        navMenu.classList.toggle('header-nav__menu--mobileActive');
-
+        navMenu.classList.toggle('header-nav__menu--mobileActive'); 
+        
         // ANIMATE LINKS ON LOAD 
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
@@ -39,15 +43,15 @@ const mobileOpen = () => {
         // ANIMATE BURGER MENU W/ TOGGLE
         mobileNav.classList.toggle('header-nav__burger--toExit');
     })
-
+    
 };
 mobileOpen();
 
 // SCROLL TO TOP ON PAGE RELOAD =============================
 
 const app = () => {
-
-    window.addEventListener('load', scrollTo(0, 0));
+    
+    window.addEventListener('load', scrollTo(0,0));
 
 }
 app();
